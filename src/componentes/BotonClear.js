f89f5b4b24f3368fe84d8ec9d1d5dc1d0c78630e
mel212333 <melina212333@gmail.com>
2024-10-28 10:34:54 -0300

    -

---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/BotonClear.css";
+const BotonClear = (props) => (
+  <div className="boton-clear" onClick={props.manejarClear}>
+    {props.children}
+  </div>
+);
+export default BotonClear;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,5 @@
+import React from "react";
+import '../style/Pantalla.css'
+const Pantalla = ({ input }) => <div className="input">{input}</div>;
+
+export default Pantalla;
